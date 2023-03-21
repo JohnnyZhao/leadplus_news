bf14c8a90b8cc4f4fb5d47dabd84b10fc230f2ef
ZhaoYu <zhaoyu.johnny@gmail.com>
2023-03-21 12:44:07 +0900

    added unique field md5 to NewsItem Model
    md5 calculated out for the original url of each news article
    to make sure no duplicate articles

--- news/views.py
+++ news/views.py
@@ -3,19 +3,23 @@ from django.utils import timezone
 from django.http import JsonResponse
 from .models import NewsItem
 
+
 class LatestNews(ListView):
-    #render the latest 20 news entries as a html page
+    """render the latest 20 news entries as a html page"""
     model = NewsItem
     template_name = 'news/latest.html'
     context_object_name = 'news_items'
     paginate_by = 20
 
-
     def get_queryset(self):
-        return NewsItem.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
+        """Get latest 20 news"""
+        return NewsItem.objects.filter(
+            published_at__lte=timezone.now()).order_by('-published_at')
+
 
 def news_items_api(request):
-    #return the latest 100 news entries as JSON
+    """Get latest 100 news"""
+    # return the latest 100 news entries as JSON
     news_items = NewsItem.objects.order_by("-published_at")[:100]
     data = {"news_items": list(news_items.values())}
     return JsonResponse(data)
